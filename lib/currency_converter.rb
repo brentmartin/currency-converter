6de76182c4a3ff41d58ad70d62b61af483deaff2
Brent Martin <bmart35@gmail.com>
2016-03-18 16:12:42 -0500

    completed currency conversion test

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -7,6 +7,14 @@ class CurrencyConverter
   end
 
   def converter(currency, code)
+    currency_amount = currency.amount
+    currency_code = currency.code
+    new_code = code
+
+    currency_rate_1 = @rate_library.fetch(new_code)
+    converted_amount_1 = currency_amount * currency_rate_1
+
+    converted_amount_2 = currency_amount * currency_rate_2
 
   end
 
